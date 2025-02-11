5c6acad367ffe111e07c1b92f353b7aaf2e20d45
Adahan00-kg <20ikramidinovrinat07@gmail.com>
2025-02-11 20:52:25 +0600

    post_with_content and comment

--- mysite/post/views.py
+++ mysite/post/views.py
@@ -6,15 +6,12 @@ from .models import *
 class PostCreateAPIView(generics.CreateAPIView):
     serializer_class = PostCreateSerializer
 
-class PostListAPIView(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostListSerializer
+
 
 class PostContentCreateAPIView(generics.CreateAPIView):
     serializer_class = PostContentPostSerializer
-#
-#
-# class PostContentListAPIView(generics.ListAPIView):
-#     queryset = PostContent.objects.all()
-#     serializer_class = PostContentListSerializer
 
+
+class PostListAPIView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
