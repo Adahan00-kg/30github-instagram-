8f8830d19bf1063d0a101dd03523176bcb7dcbab
Adahan00-kg <20ikramidinovrinat07@gmail.com>
2025-02-19 22:32:26 +0600

    chat cunsomers and models

---
+++ mysite/chat/cunsomers.py
@@ -0,0 +1,63 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import Chat, Message, UserProfile
+from asgiref.sync import sync_to_async
+
+
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.chat_id = self.scope['url_route']['kwargs']['chat_id']
+        self.chat_group_name = f'chat_{self.chat_id}'
+
+        # Присоединяемся к группе чата
+        await self.channel_layer.group_add(
+            self.chat_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Покидаем группу чата
+        await self.channel_layer.group_discard(
+            self.chat_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        author_id = data['author']
+        text = data.get('text', '')
+
+        author = await sync_to_async(UserProfile.objects.get)(id=author_id)
+        chat = await sync_to_async(Chat.objects.get)(id=self.chat_id)
+
+        # Создаём сообщение в БД
+        message = await sync_to_async(Message.objects.create)(
+            chat=chat,
+            author=author,
+            text=text
+        )
+
+        message_data = {
+            'id': message.id,
+            'chat': self.chat_id,
+            'author': author.id,
+            'text': text,
+            'created_at': str(message.created_at)
+        }
+
+        # Отправляем сообщение всем участникам чата
+        await self.channel_layer.group_send(
+            self.chat_group_name,
+            {
+                'type': 'chat_message',
+                'message': message_data
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps(message))
--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(AbstractUser):
+    bio = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='user_image', null=True, blank=True)
+    website = models.URLField(null=True, blank=True)
+
+    def str(self):
+        return self.username
+
+
+class Chat(models.Model):
+    person = models.ManyToManyField(UserProfile)
+    created_date = models.DateField(auto_now_add=True)
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='message')
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    text = models.CharField(max_length=355, null=True, blank=True)
+    image = models.ImageField(upload_to='message_image', null=True, blank=True)
+    video = models.FileField(upload_to='message_video', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
